ca20326205bfaa71ac6b8f27cd11108603b7bfc9
Srikanth Tirandas <stirandas@gmail.com>
2025-10-01 19:52:24 +0530

    Your commit message here

---
+++ db_design.sql
@@ -0,0 +1,9 @@
+CREATE TABLE t_nse_fii_dii_eq_data (
+  run_dt   date PRIMARY KEY,
+  dii_buy  numeric(9,2),
+  dii_sell numeric(9,2),
+  dii_net  numeric(7,2),
+  fii_buy  numeric(9,2),
+  fii_sell numeric(9,2),
+  fii_net  numeric(7,2)
+);

a06f00c241bf2ccb7cda2ebf492685090d7cdf10
Srikanth Tirandas <stirandas@gmail.com>
2025-10-14 20:54:05 +0530

    Adding correct db_design.sql

--- db_design.sql
+++ db_design.sql
@@ -1,10 +1,11 @@
 CREATE TABLE t_nse_fii_dii_eq_data (
   run_dt   date PRIMARY KEY,
-  dii_buy  numeric(9,2),
-  dii_sell numeric(9,2),
-  dii_net  numeric(7,2),
-  fii_buy  numeric(9,2),
-  fii_sell numeric(9,2),
-  fii_net  numeric(7,2),
-  iu_ts    timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP
+  dii_buy  numeric(9,2) NOT NULL,
+  dii_sell numeric(9,2) NOT NULL,
+  dii_net  numeric(7,2) NOT NULL,
+  fii_buy  numeric(9,2) NOT NULL,
+  fii_sell numeric(9,2) NOT NULL,
+  fii_net  numeric(7,2) NOT NULL,
+  i_ts    timestamptz,
+  u_ts    timestamptz
 );
